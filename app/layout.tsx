a202697d3435ffab6156c16cfb1d206055a9a01b
Mintostar <kazukihokama@gmail.com>
2025-04-23 16:09:28 +0900

    - shadcn/uiを入れた - ライブラリreact-simple-typewriter,frame-motionを追加
    Note: react-simple-typewriterはタイピング風のアニメーションを管理するために追加。frame-motionはボタンが浮き上がってくるようなアニメーションのために導入。両方とも未実装。

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,30 @@
 import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "This is the portfolio site of XXX.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
